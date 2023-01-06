ee741843fd832c20e3566e5f67c14c9ff144bc09
rayden <rayden@tut.by>
2023-01-06 21:32:01 +0300

    Initial commit

---
+++ src/main/java/com/example/securdemoclient/SecurDemoClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.securdemoclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurDemoClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurDemoClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/securdemoclient/SecurDemoClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securdemoclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurDemoClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

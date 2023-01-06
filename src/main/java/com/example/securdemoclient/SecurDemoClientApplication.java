4dead8f4cbfe9850ae86448e8fff0959a2a6226a
rayden <rayden@tut.by>
2023-01-06 21:34:40 +0300

    Removed unnecessary stuff

--- src/main/java/com/example/securdemoclient/SecurDemoClientApplication.java
+++ src/main/java/com/example/securdemoclient/SecurDemoClientApplication.java
@@ -2,8 +2,10 @@ package com.example.securdemoclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SecurDemoClientApplication {
 
     public static void main(String[] args) {

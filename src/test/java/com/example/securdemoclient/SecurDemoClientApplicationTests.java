c580074bc225871a86b1805cd17e44169b7fe4cc
rayden <rayden@tut.by>
2023-01-23 19:34:35 +0300

    Fixed "contextLoads" integration test.

--- src/test/java/com/example/securdemoclient/SecurDemoClientApplicationTests.java
+++ src/test/java/com/example/securdemoclient/SecurDemoClientApplicationTests.java
@@ -2,8 +2,16 @@ package com.example.securdemoclient;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 @SpringBootTest
+
+// We had to use a mocked bean to avoid the error on the application context loading:
+// "Unable to resolve Configuration with the provided Issuer of "https://mydebian.local:8443/realms/PetClinicRearm""
+// See application.yml: spring.security.oauth2.client.provider.keycloak.issuer-uri
+@MockBean(ClientRegistrationRepository.class)
+
 class SecurDemoClientApplicationTests {
 
     @Test
